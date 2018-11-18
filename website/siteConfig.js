70554eb8709bb53d32d341a6f34700cc5606402e
jdecroock <decroockjovi@gmail.com>
2018-11-18 19:17:27 +0100

    docs: change punch line

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -8,21 +8,9 @@
 // See https://docusaurus.io/docs/site-config for all the possible
 // site configuration options.
 
-// List of projects/orgs using your project for the users page.
-const users = [
-  {
-    caption: 'User1',
-    // You will need to prepend the image path with your baseUrl
-    // if it is not '/', like: '/test-site/img/docusaurus.svg'.
-    image: '/img/docusaurus.svg',
-    infoLink: 'https://www.facebook.com',
-    pinned: true,
-  },
-];
-
 const siteConfig = {
   title: 'Hooked-Form',
-  tagline: 'Hooked-Form documentation.',
+  tagline: "You will get hooked.",
   url: 'https://jovidecroock.github.io',
   baseUrl: '/hooked-form/',
   projectName: 'hooked-form',
@@ -32,9 +20,6 @@ const siteConfig = {
   headerLinks: [
     { doc: 'components/form', label: 'API' },
   ],
-
-  // If you have users set above, you add it here:
-  users,
 
   /* path to images for header/footer */
   headerIcon: 'img/logo.png',
