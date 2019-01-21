ecee4190489882979c4f76e4a4405beb9c2e0a56
Jovi De Croock <jovi@codifly.be>
2019-01-21 17:52:25 +0100

    __DEV__ for Error

--- src/Error.tsx
+++ src/Error.tsx
@@ -8,8 +8,10 @@ export interface FieldProps {
 }
 
 const ErrorContainer: React.FC<FieldProps> = React.memo(({ component, fieldId }) => {
-  if (!component) { throw new Error('The Field needs a "component" property to  function correctly.') }
-  if (!fieldId || typeof fieldId !== 'string') { throw new Error('The Field needs a valid "fieldId" property to  function correctly.') }
+  if (process.env.NODE_ENV !== 'production') {
+    if (!component) { throw new Error('The Field needs a "component" property to  function correctly.') }
+    if (!fieldId || typeof fieldId !== 'string') { throw new Error('The Field needs a valid "fieldId" property to  function correctly.') }
+  }
   const error = useError(fieldId)
   const props = { error }
   return React.useMemo(() => React.createElement(component, props), [error])
