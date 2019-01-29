0ef5e08a5abf1ec904db1f889a6739e3453a92d3
Jovi De Croock <jovi@codifly.be>
2019-01-29 16:30:28 +0100

    chore: cleanup FieldArray.tsx

--- src/FieldArray.tsx
+++ src/FieldArray.tsx
@@ -11,7 +11,6 @@ export interface FieldProps {
 const FieldArrayContainer: React.FC<FieldProps> = React.memo(({ component, render, fieldId, ...rest }) => {
   if (process.env.NODE_ENV !== 'production') {
     if (!component && !render) { throw new Error('The FieldArray needs a "component" or a "render" property to  function correctly.') }
-    if (!fieldId || typeof fieldId !== 'string') { throw new Error('The FieldArray needs a valid "fieldId" property to  function correctly.') }
   }
   const {
     0: {
