074ac0c6b010f1776f0f7e846c976d2ee7871913
Jovi De Croock <jovi@codifly.be>
2019-01-29 16:30:09 +0100

    refactor: cleanup Error.tsx

--- src/Error.tsx
+++ src/Error.tsx
@@ -10,11 +10,9 @@ export interface FieldProps {
 const ErrorContainer: React.FC<FieldProps> = React.memo(({ component, fieldId }) => {
   if (process.env.NODE_ENV !== 'production') {
     if (!component) { throw new Error('The Field needs a "component" property to  function correctly.') }
-    if (!fieldId || typeof fieldId !== 'string') { throw new Error('The Field needs a valid "fieldId" property to  function correctly.') }
   }
   const error = useError(fieldId)
-  const props = { error }
-  return React.useMemo(() => React.createElement(component, props), [error])
+  return React.useMemo(() => React.createElement(component, { error }), [error])
 }, () => true)
 
 export default ErrorContainer
