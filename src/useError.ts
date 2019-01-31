5f0adfbea61e23bb668400168aeaa03bf9b6f1ac
Jovi De Croock <jovi@codifly.be>
2019-01-31 10:18:36 +0100

    chore: optimise useError

--- src/useError.ts
+++ src/useError.ts
@@ -7,6 +7,9 @@ export interface FieldInformation {
 }
 
 export default function useError(fieldId: string): string | null {
+  if (process.env.NODE_ENV !== 'production') {
+    if (!fieldId || typeof fieldId !== 'string') { throw new Error('The FieldArray needs a valid "fieldId" property to  function correctly.') }
+  }
   const { errors } = React.useContext(formContext)
   const error = React.useMemo(() => get(errors, fieldId), [errors])
   return error
