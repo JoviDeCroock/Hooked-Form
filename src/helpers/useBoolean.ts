cf039bd2533d31a4faa17ee6b12a87d44452c893
Jovi De Croock <jovi@codifly.be>
2019-02-01 15:22:48 +0100

    chore: update useBool

--- src/helpers/useBoolean.ts
+++ src/helpers/useBoolean.ts
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 
-export default (initial: boolean = true): [(value: boolean) => void, boolean] => {
-  const { 0: value, 1: setValue } = useState(initial)
+export default (initial: boolean): [(value: boolean) => void, boolean] => {
+  const { 0: value, 1: setValue } = useState(initial || false)
   return [setValue, value]
 }
