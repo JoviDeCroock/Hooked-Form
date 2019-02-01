e61841ea92e73259ed5887aa1a9cd4d8a9b3be3d
Jovi De Croock <jovi@codifly.be>
2019-02-01 15:22:58 +0100

    chore: useState

--- src/helpers/useState.ts
+++ src/helpers/useState.ts
@@ -1,8 +1,8 @@
 import * as React from 'react'
 import { set } from './operations'
 
-export default (initial: object = {}): [object, (id: string, value: any) => void, (newState: object) => void] => {
-  const { 0: values, 1: innerSetValue } = React.useState(initial)
+export default (initial: object): [object, (id: string, value: any) => void, (newState: object) => void] => {
+  const { 0: values, 1: innerSetValue } = React.useState(initial || {})
   const setValue = React.useCallback((id: string, value: any) => innerSetValue((state) => set(state, id, value)), [innerSetValue])
   const setState = React.useCallback((newState: object) => innerSetValue(() => ({ ...newState })), [innerSetValue])
   return [values, setValue, setState]
