0215fb8c03e57ba7e9f33014a191d30e98e387f2
Jovi De Croock <jovi@codifly.be>
2019-02-01 15:22:20 +0100

    chore: update semantic of operations

--- src/helpers/operations.ts
+++ src/helpers/operations.ts
@@ -4,7 +4,10 @@ export interface Source {
   [key: string]: any
 }
 
-export const get = (source: Source, key: any) => getHelper(source, toPath(key), 0)
+export function get(source: Source, key: any): any {
+  return getHelper(source, toPath(key), 0)
+}
+
 function getHelper(source: Source, path: Array<string>, index: number): any {
   if (!source || path.length <= index) {
     return source
@@ -12,8 +15,10 @@ function getHelper(source: Source, path: Array<string>, index: number): any {
   return getHelper(source[path[index]], path, index + 1)
 }
 
-export const set = (source: Source | Array<any>, key: string, value: any) => setHelper(source, value, toPath(key), 0)
-function setHelper(source: Source | Array<any>, value: any, pathArray: Array<string>, currentIndex: number) {
+export function set(source: Source | Array<any>, key: string, value: any): any {
+  return setHelper(source, value, toPath(key), 0)
+}
+function setHelper(source: Source | Array<any>, value: any, pathArray: Array<string>, currentIndex: number): any {
   if (currentIndex >= pathArray.length) { return value }
   const currentPath = pathArray[currentIndex]
   // At this point we could be dealing with a FieldArray so be cautious not to use Stringed keys, if not it's an object.
