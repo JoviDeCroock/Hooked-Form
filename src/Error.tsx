8f1c4877d535c4abe08f60a9c1b7642897cf414b
jdecroock <decroockjovi@gmail.com>
2019-02-03 11:57:17 +0100

    pass props from Error

--- src/Error.tsx
+++ src/Error.tsx
@@ -4,15 +4,15 @@ import useError from './useError'
 export interface FieldProps {
   component: any
   fieldId: string
-  [x: string]: any
+  [additionalProps: string]: any
 }
 
-const ErrorContainer: React.FC<FieldProps> = React.memo(({ component, fieldId }) => {
+const ErrorContainer: React.FC<FieldProps> = React.memo(({ component, fieldId, ...rest }) => {
   if (process.env.NODE_ENV !== 'production') {
     if (!component) { throw new Error('The Field needs a "component" property to  function correctly.') }
   }
   const error = useError(fieldId)
-  return React.useMemo(() => React.createElement(component, { error }), [error])
+  return React.useMemo(() => React.createElement(component, { error, ...rest }), [error])
 }, () => true)
 
 export default ErrorContainer
