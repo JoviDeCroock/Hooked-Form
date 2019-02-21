59a4823c1475bab06bc4ae7c2518e5cdc7346112
Jovi De Croock <jovi@codifly.be>
2019-02-21 15:10:01 +0100

    refactor: remove unused file

--- src/helpers/useBoolean.ts
+++
@@ -1,6 +0,0 @@
-import { useState } from 'react'
-
-export default (initial: boolean): [(value: boolean) => void, boolean] => {
-  const { 0: value, 1: setValue } = useState(initial || false)
-  return [setValue, value]
-}
