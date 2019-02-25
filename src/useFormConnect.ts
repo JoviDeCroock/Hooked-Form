cf2c8eb3045395fdea231179e4060a81b4a060c4
jdecroock <decroockjovi@gmail.com>
2019-02-25 17:17:24 +0100

    chore: offer new way to fetch all form vars

---
+++ src/useFormConnect.ts
@@ -0,0 +1,9 @@
+import * as React from 'react';
+import { formContext } from './helpers/context';
+import { FormHookContext } from './types';
+
+const useFormConnect = ():FormHookContext => {
+  return React.useContext(formContext);
+}
+
+export default useFormConnect;
