56de00a1ec60368285a881c95abf281a59c3e9e2
jdecroock <decroockjovi@gmail.com>
2019-02-25 21:18:28 +0100

    feat: strip annoying license

---
+++ scripts/sliceLicense.js
@@ -0,0 +1,11 @@
+var fs = require('fs');
+
+const filename = `${process.cwd()}/dist/hooked-form.modern.js`;
+
+fs.readFile(filename, 'utf8', function(err, data) {
+  const lines = data.split('\n');
+  const imports = lines.slice(0, 1);
+  const source = lines.slice(16);
+  const final = [...imports, ...source].join('\n');
+  fs.writeFile(filename, final, console.log);
+});
