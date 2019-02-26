6427574095a3e1183ac604a53a633e984e3cd99b
jdecroock <decroockjovi@gmail.com>
2019-02-26 16:58:57 +0100

    feat: derive initial with arrays

--- src/helpers/deriveInitial.ts
+++ src/helpers/deriveInitial.ts
@@ -1,7 +1,9 @@
 export function deriveInitial(input: { [fieldId: string]: any }, defaultValue: any): { [fieldId: string]: any } {
   return Object.keys(input).reduce((acc: { [fieldId: string]: any }, key: string) => {
-    if (typeof key === 'object') {
-      return { ...acc, [key]: { ...deriveInitial(input[key], defaultValue) } }
+    if (Array.isArray(input[key])) {
+      return { ...acc, [key]: input[key].map((value: any) => deriveInitial(value, defaultValue)) }
+    } else if (typeof input[key] === 'object') {
+      return { ...acc, [key]: deriveInitial(input[key], defaultValue) }
     } else {
       return { ...acc, [key]: defaultValue }
     }
