fb3ed02e5215b0ccd2d836346752f61dff031ff4
jdecroock <decroockjovi@gmail.com>
2019-03-02 20:03:44 +0100

    remove logs

--- src/helpers/deriveInitial.ts
+++ src/helpers/deriveInitial.ts
@@ -1,9 +1,7 @@
 export function deriveInitial(
   input: { [fieldId: string]: any }, defaultValue: any,
 ): { [fieldId: string]: any } {
-  console.log('input', input);
   return Object.keys(input).reduce((acc: { [fieldId: string]: any }, key: string) => {
-    console.log('reduce', key, input[key]);
     if (Array.isArray(input[key])) {
       acc[key] = input[key].map((value: any) => deriveInitial(value, defaultValue));
       return acc;
