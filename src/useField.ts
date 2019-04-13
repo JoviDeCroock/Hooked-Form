3fcde9cbeb5b02d093d58017e71a3661d629ff8a
jdecroock <decroockjovi@gmail.com>
2019-04-13 09:26:10 +0200

    bundle size savings

--- src/useField.ts
+++ src/useField.ts
@@ -32,20 +32,23 @@ export default function useField(fieldId: string): [FieldOperations, FieldInform
     touched,
   } = React.useContext(formContext);
   // Values
-  const error = React.useMemo(() => get(errors, fieldId), [errors]);
   const initialValue = React.useMemo(() => get(initialValues, fieldId), [initialValues]);
   const value = React.useMemo(() => get(values, fieldId), [values]);
-  const isFieldTouched = React.useMemo(() => get(touched, fieldId), [touched]);
-  // Methods
-  const reset = React.useCallback(() => {
-    setFieldValue(fieldId, initialValue || baseReset(value));
-    setFieldTouched(fieldId, false);
-  }, []);
-  const onChange = React.useCallback((val: any) => setFieldValue(fieldId, val), []);
-  const onBlur = React.useCallback(() => setFieldTouched(fieldId, true), []);
-  const onFocus = React.useCallback(() => setFieldTouched(fieldId, false), []);
   return [
-    { onChange, onBlur, onFocus, reset, setFieldValue },
-    { error, touched: isFieldTouched, value },
+    {
+      onBlur: React.useCallback(() => setFieldTouched(fieldId, true), []),
+      onChange: React.useCallback((val: any) => setFieldValue(fieldId, val), []),
+      onFocus: React.useCallback(() => setFieldTouched(fieldId, false), []),
+      reset: React.useCallback(() => {
+        setFieldValue(fieldId, initialValue || baseReset(value));
+        setFieldTouched(fieldId, false);
+      }, []),
+      setFieldValue,
+    },
+    {
+      error: React.useMemo(() => get(errors, fieldId), [errors]),
+      touched: React.useMemo(() => get(touched, fieldId), [touched]),
+      value,
+    },
   ];
 }
