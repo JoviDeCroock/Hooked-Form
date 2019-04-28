9a2b19691cfc5ea839dc7091b1a054bb1646beb7
jdecroock <decroockjovi@gmail.com>
2019-04-28 13:27:47 +0200

    refactor: helper golfing

--- src/helpers/toPath.ts
+++ src/helpers/toPath.ts
@@ -1,28 +1,25 @@
-export function memoize(func: (input: string) => Array<string>) {
-  let resultMapping: { [input: string]: Array<string> } = {};
+function makeToPathArrayConverter() {
+  let resultMapping: { [value: string]: Array<string> } = {};
   let count = 0;
-  return function memoizedFunc(input: string) {
-    if (!resultMapping[input]) {
-      resultMapping[input] = func(input);
-      count = count + 1;
-      if (count >= 600) { resultMapping = {}; }
-    }
-    return resultMapping[input];
+
+  return function toPathArray(value: string) {
+    if (resultMapping[value]) return resultMapping[value];
+    const result: Array<string> = [];
+
+    value.split('.').forEach((part: string) => {
+      if (part.includes('[')) {
+        const { 0: firstPart, 1: temp } = part.split('[');
+        result.push(firstPart);
+        const { 0: secondPart } = temp.split(']');
+        result.push(secondPart);
+      } else result.push(part);
+    });
+
+    if (count >= 600) { resultMapping = {}; }
+    count++; // tslint:disable-line
+
+    return resultMapping[value] = result;
   };
 }
 
-const toPathArray = (input: string) => {
-  const parts = input.split('.');
-  const result: Array<string> = [];
-  parts.forEach((part: string) => {
-    if (part.includes('[')) {
-      const { 0: firstPart, 1: temp } = part.split('[');
-      result.push(firstPart);
-      const { 0: secondPart } = temp.split(']');
-      result.push(secondPart);
-    } else { result.push(part); }
-  });
-  return result;
-};
-
-export default memoize(toPathArray);
+export default makeToPathArrayConverter();
