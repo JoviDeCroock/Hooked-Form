4a8d7cb4a07302318aa72910f7cf2071b917fa9c
Jovi De Croock <decroockjovi@gmail.com>
2019-05-05 09:48:38 +0200

    feat: export them all

---
+++ preact/src/index.ts
@@ -0,0 +1,8 @@
+export { default as Form } from './Form';
+export { default as Field } from './Field';
+export { default as useField } from './useField';
+export { default as FieldArray } from './FieldArray';
+export { default as useFieldArray } from './useFieldArray';
+export { default as ErrorMessage } from './Error';
+export { default as useError } from './useError';
+export { default as useFormConnect } from './useFormConnect';
