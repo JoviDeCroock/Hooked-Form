f7be2fb533499e94154879d5bd575f437e542359
jdecroock <decroockjovi@gmail.com>
2019-05-20 17:08:32 +0200

    push all at once

--- src/helpers/toPath.ts
+++ src/helpers/toPath.ts
@@ -8,9 +8,8 @@ export default (() => {
     value.split('.').forEach((part: string) => {
       if (part.includes('[')) {
         const { 0: firstPart, 1: temp } = part.split('[');
-        result.push(firstPart);
         const { 0: secondPart } = temp.split(']');
-        result.push(secondPart);
+        result.push(firstPart, secondPart);
       } else result.push(part);
     });
 
