d072c875283633a256304c279df51317058a2d87
jdecroock <decroockjovi@gmail.com>
2019-05-23 17:21:55 +0200

    chore: optimise toPath

--- src/helpers/toPath.ts
+++ src/helpers/toPath.ts
@@ -1,18 +1,7 @@
 export default (() => {
   const resultMapping: { [value: string]: Array<string> } = {};
-
   return function toPathArray(value: string) {
     if (resultMapping[value]) return resultMapping[value];
-    const result: Array<string> = [];
-
-    value.split('.').forEach((part: string) => {
-      if (part.includes('[')) {
-        const { 0: firstPart, 1: temp } = part.split('[');
-        const { 0: secondPart } = temp.split(']');
-        result.push(firstPart, secondPart);
-      } else result.push(part);
-    });
-
-    return resultMapping[value] = result;
+    return resultMapping[value] = value.replace(/\[("|')?([^\[\]]+)\1\]/g, '.$2').split('.');
   };
 })();
