6acec2705c2bd4ce024b61921099d1e9fd7efd48
Jovi De Croock <decroockjovi@gmail.com>
2019-07-20 12:07:04 +0200

    (chore) - no closure compiler for alpha release

--- rollup.config.js
+++ rollup.config.js
@@ -22,38 +22,39 @@ const config = [
       typescriptPlugin({ typescript, tsconfig: './tsconfig.json' }),
       filesize(),
     ],
-   }, {
-    input: 'src/index.ts',
-    output: {
-      file: 'dist/prod/hooked-form.modern.js',
-      format: 'esm',
-      sourcemap: true,
-    },
-    plugins: [
-      replace({ 'process.env.NODE_ENV': JSON.stringify('production') }),
-      nodeResolve({
-        mainFields: ['module'],
-        only: ['tslib']
-      }),
-      typescriptPlugin({ typescript, tsconfig: './tsconfig.json', objectHashIgnoreUnknownHack: true }),
-      compiler({ compilation_level: 'ADVANCED_OPTIMIZATIONS' }),
-      terser({
-        sourcemap: true,
-        output: { comments: false },
-        compress: {
-          keep_infinity: true,
-          pure_getters: true,
-          passes: 10,
-          global_defs: { 'process.env.NODE_ENV': 'production' }
-        },
-        warnings: true,
-        ecma: 6,
-        toplevel: true,
-        mangle: { properties: '^_' },
-      }),
-      filesize(),
-    ],
    },
+  //  {
+  //   input: 'src/index.ts',
+  //   output: {
+  //     file: 'dist/prod/hooked-form.modern.js',
+  //     format: 'esm',
+  //     sourcemap: true,
+  //   },
+  //   plugins: [
+  //     replace({ 'process.env.NODE_ENV': JSON.stringify('production') }),
+  //     nodeResolve({
+  //       mainFields: ['module'],
+  //       only: ['tslib']
+  //     }),
+  //     typescriptPlugin({ typescript, tsconfig: './tsconfig.json', objectHashIgnoreUnknownHack: true }),
+  //     compiler({ compilation_level: 'ADVANCED_OPTIMIZATIONS' }),
+  //     terser({
+  //       sourcemap: true,
+  //       output: { comments: false },
+  //       compress: {
+  //         keep_infinity: true,
+  //         pure_getters: true,
+  //         passes: 10,
+  //         global_defs: { 'process.env.NODE_ENV': 'production' }
+  //       },
+  //       warnings: true,
+  //       ecma: 6,
+  //       toplevel: true,
+  //       mangle: { properties: '^_' },
+  //     }),
+  //     filesize(),
+  //   ],
+  //  },
 ];
 
 export default config;
