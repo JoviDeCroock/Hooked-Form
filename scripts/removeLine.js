24f802b57b985e03270b3e4b63950eefe351f1ea
jdecroock <decroockjovi@gmail.com>
2019-07-21 21:22:35 +0200

    (chore) - make build work

---
+++ scripts/removeLine.js
@@ -0,0 +1,13 @@
+var fs = require('fs');
+
+const filename = `${process.cwd()}/node_modules/microbundle/dist/microbundle.js`;
+const IGNORED_LINE = 647;
+
+fs.readFile(filename, 'utf8', function(err, data) {
+  let lines = data.split('\n')
+  lines = lines.filter((x, i) => i !== IGNORED_LINE && !x.includes("browser: options.target !== 'node'"))
+  console.log('hi', lines[712])
+  lines[712] = "fs.writeFileSync(path.resolve(options.cwd, 'mangle.json'), JSON.stringify(nameCache, null, 2));"
+  console.log('hi', lines[712])
+  fs.writeFileSync(filename, lines.join('\n'));
+});
