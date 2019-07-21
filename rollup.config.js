91828cbf0c19bdb604d45699c22f3787dcd4cb55
jdecroock <decroockjovi@gmail.com>
2019-07-21 21:39:46 +0200

    (chore) - add modern prod bundle

--- rollup.config.js
+++ rollup.config.js
@@ -2,7 +2,7 @@ import nodeResolve from 'rollup-plugin-node-resolve';
 import filesize from 'rollup-plugin-filesize';
 import typescript from 'typescript';
 import typescriptPlugin from 'rollup-plugin-typescript2';
-import compiler from '@ampproject/rollup-plugin-closure-compiler';
+// import compiler from '@ampproject/rollup-plugin-closure-compiler';
 import { terser } from 'rollup-plugin-terser';
 import replace from 'rollup-plugin-replace';
 
@@ -24,38 +24,38 @@ const config = [
       filesize(),
     ],
    },
-  //  {
-  //   input: 'src/index.ts',
-  //   output: {
-  //     file: 'dist/prod/hooked-form.modern.js',
-  //     format: 'esm',
-  //     sourcemap: true,
-  //   },
-  //   plugins: [
-  //     replace({ 'process.env.NODE_ENV': JSON.stringify('production') }),
-  //     nodeResolve({
-  //       mainFields: ['module', 'jsnext', 'main'],
-  //       only: ['tslib', 'use-context-selector']
-  //     }),
-  //     typescriptPlugin({ typescript, tsconfig: './tsconfig.json', objectHashIgnoreUnknownHack: true }),
-  //     compiler({ compilation_level: 'ADVANCED_OPTIMIZATIONS' }),
-  //     terser({
-  //       sourcemap: true,
-  //       output: { comments: false },
-  //       compress: {
-  //         keep_infinity: true,
-  //         pure_getters: true,
-  //         passes: 10,
-  //         global_defs: { 'process.env.NODE_ENV': 'production' }
-  //       },
-  //       warnings: true,
-  //       ecma: 6,
-  //       toplevel: true,
-  //       mangle: { properties: '^_' },
-  //     }),
-  //     filesize(),
-  //   ],
-  //  },
+   {
+    input: 'src/index.ts',
+    output: {
+      file: 'dist/prod/hooked-form.modern.js',
+      format: 'esm',
+      sourcemap: true,
+    },
+    plugins: [
+      replace({ 'process.env.NODE_ENV': JSON.stringify('production') }),
+      nodeResolve({
+        mainFields: ['module', 'jsnext', 'main'],
+        only: ['tslib', 'use-context-selector']
+      }),
+      typescriptPlugin({ typescript, tsconfig: './tsconfig.json', objectHashIgnoreUnknownHack: true }),
+      // compiler({ compilation_level: 'ADVANCED_OPTIMIZATIONS' }),
+      terser({
+        sourcemap: true,
+        output: { comments: false },
+        compress: {
+          keep_infinity: true,
+          pure_getters: true,
+          passes: 10,
+          global_defs: { 'process.env.NODE_ENV': 'production' }
+        },
+        warnings: true,
+        ecma: 6,
+        toplevel: true,
+        mangle: { properties: '^_' },
+      }),
+      filesize(),
+    ],
+   },
 ];
 
 export default config;
