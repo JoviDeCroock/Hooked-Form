77f8e5bce44102a7661457244cc2ad6343af9380
Jovi De Croock <jovi.decroock@formidable.com>
2019-10-19 11:17:05 +0200

    (chore) - cut some size in helpers

--- src/helpers/context.ts
+++ src/helpers/context.ts
@@ -2,5 +2,3 @@ import { createContext } from '../context/createContext';
 import { FormHookContext } from '../types';
 
 export const formContext = createContext<FormHookContext>();
-const { Provider } = formContext;
-export { Provider };
