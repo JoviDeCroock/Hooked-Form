5c64a9320789e13e2aee861813d23bcd2daec209
jdecroock <decroockjovi@gmail.com>
2020-01-18 13:38:47 +0100

    update useSpy

--- preact/src/useSpy.ts
+++ preact/src/useSpy.ts
@@ -3,12 +3,21 @@ import { get } from './helpers/operations';
 import { FormHookContext } from './types';
 import { useFormConnect } from './useFormConnect';
 
-export const useSpy = (fieldId: string, cb: (newValue: any, ctx: FormHookContext) => void) => {
-  const isMounted = useRef<undefined | boolean>();
+export const useSpy = (
+  fieldId: string,
+  cb: (newValue: any, ctx: FormHookContext) => void
+) => {
+  const isMounted = useRef<boolean | undefined>();
   const ctx = useFormConnect();
+  const value = get(ctx.values, fieldId);
   useEffect(() => {
     if (isMounted.current) cb(get(ctx.values, fieldId), ctx);
-    // @ts-ignore
-    isMounted.current = true;
+    (isMounted as any).current = true;
   }, [get(ctx.values, fieldId)]);
+
+  return {
+    error: get(ctx.errors, fieldId),
+    touched: get(ctx.touched, fieldId),
+    value,
+  };
 };
