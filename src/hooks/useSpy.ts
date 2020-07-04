b20321d6913e8dea6bfb048e172d19c3ebdd8e6c
Jovi De Croock <decroockjovi@gmail.com>
2020-07-04 02:03:44 +0200

    golf useSpy

--- src/hooks/useSpy.ts
+++ src/hooks/useSpy.ts
@@ -9,14 +9,12 @@ export default function useSpy<T = any>(
   fieldId: string,
   cb?: SpyCallback<T>
 ): FieldInformation<T> {
-  const isMounted = React.useRef<boolean>(false);
   const ctx = React.useContext(formContext);
   const value = get(ctx.values, fieldId);
-  const prev = React.useRef<T>(value);
+  const prev = React.useRef<T>();
 
   React.useEffect(() => {
-    if (isMounted.current && cb && prev.current !== value) cb(value, ctx);
-    isMounted.current = true;
+    if (prev.current && cb && prev.current !== value) cb(value, ctx);
     prev.current = value;
   }, [value]);
 
