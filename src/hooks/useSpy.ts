4b3f07af950de8808f945ee003c85c24c6c1a7c0
jdecroock <decroockjovi@gmail.com>
2020-07-11 10:35:37 +0200

    fix spy

--- src/hooks/useSpy.ts
+++ src/hooks/useSpy.ts
@@ -9,12 +9,14 @@ export default function useSpy<T = any>(
   fieldId: string,
   cb?: SpyCallback<T>
 ): FieldInformation<T> {
+  const isMounted = React.useRef<boolean>(false);
   const ctx = React.useContext(formContext);
   const value = get(ctx.values, fieldId);
   const prev = React.useRef<T>();
 
   React.useEffect(() => {
-    if (prev.current && cb && prev.current !== value) cb(value, ctx);
+    if (isMounted.current && cb && prev.current !== value) cb(value, ctx);
+    isMounted.current = true;
     prev.current = value;
   }, [value]);
 
